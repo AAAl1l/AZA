4a8022945d3dffa67a69de28000e1e25855e44a6
AAAl1l <abasalmadride@gmail.com>
2024-06-21 02:57:21 +0300

    Update __init__.py

--- zelz/helpers/functions/__init__.py
+++ zelz/helpers/functions/__init__.py
@@ -2,7 +2,6 @@ from .findquote import *
 from .functions import *
 from .ialivetext import *
 from .imgtools import *
-from .jikan import *
 from .nekos import *
 from .utils import *
 from .vidtools import *
